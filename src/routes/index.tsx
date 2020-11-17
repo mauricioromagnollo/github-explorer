29d5dc3224d82e3422ca237baa57c6ff31218792
x0n4d0 <romagnolimauricio@gmail.com>
2020-11-17 08:32:32 -0300

    feat: add router file

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { Dashboard } from '../pages/Dashboard';
+import { Repository } from '../pages/Repository';
+
+export const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" exact component={Repository} />
+  </Switch>
+);

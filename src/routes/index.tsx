a87dd4f1ad98d0c2b1da5689007375b21a87e0f8
x0n4d0 <romagnolimauricio@gmail.com>
2020-11-17 12:57:12 -0300

    fix: change route name repository to repositories

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,6 @@ import { Repository } from '../pages/Repository';
 export const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" exact component={Repository} />
+    <Route path="/repositories/:repository+" exact component={Repository} />
   </Switch>
 );

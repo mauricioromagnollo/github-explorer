75cf6e0695df4e090758f099482692237681ba27
x0n4d0 <romagnolimauricio@gmail.com>
2020-11-28 08:49:10 -0300

    fix: change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import { Routes } from './routes';
@@ -7,8 +7,8 @@ import { Routes } from './routes';
 export const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <BrowserRouter>
+    <HashRouter>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
   </>
 );
